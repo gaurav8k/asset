450d96414848d4eb66cb949b8a5eb67b5968d92b
Gaurav Rajkumar <gaurav8k@gmail.com>
2016-08-31 12:29:54 +0530

    First commit

---
+++ config/version.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  'app_version' => 'v3.3.0',
+  'hash_version' => 'v3.3.0-3-g7ef4f23',
+);
---
+++ resources/lang/sv-SE/admin/asset_maintenances/table.php
@@ -0,0 +1,9 @@
+<?php
+
+    return [
+        'title'         => 'Asset Maintenance',
+        'asset_name'    => 'Inventarie',
+        'supplier_name' => 'Leverantör',
+        'is_warranty'   => 'Garanti',
+        'dl_csv'        => 'Ladda ner CSV'
+    ];
